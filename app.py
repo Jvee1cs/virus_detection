304bf12bbf60548493654e2c6e6d7a12a4f5adde
Jake Baloran <111233632+Jvee1cs@users.noreply.github.com>
2025-04-28 10:16:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import base64
 import requests
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 from flask import Flask, request, jsonify
@@ -9,7 +10,21 @@ from datetime import datetime
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://virus-aics.web.app"}})
 # Load Firebase
-
+firebase_config = {
+    "type": "service_account",
+    "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+    "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+    "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+    "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+    "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL')
+}
+# Initialize Firebase with credentials
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred)
 # Initialize coupon codes collection
 COUPON_CODES = {
     "phis7d": {"MAX_USES": 3},  # Existing coupon code
